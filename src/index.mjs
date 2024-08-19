81192bca8da3039fa78ae7ca45daf9a5acb8e190
ralphfromthesky <ralphsantolorin53@gmail.com>
2024-08-19 12:27:06 +0800

    new

--- src/index.mjs
+++ src/index.mjs
@@ -1,15 +1,29 @@
 import express, { raw, response } from "express";
 import { query, validationResult, body } from "express-validator";
 import cookieParser, { signedCookie } from "cookie-parser";
+import session from "express-session";
 
 const app = express();
 //for parsing cookie or converting string to an object
-// app.use(cookieParser()) 
+// app.use(cookieParser())
 //you can also pass secret to cookieParser for authentication
-app.use(cookieParser("helloworld")) 
+app.use(cookieParser("helloworld"));
 
 //need this one declare first before using app.get/post/put/delete/patch need on top ordering is matter
 app.use(express.json()); // this one is for parsing data middleware
+
+//if youre using app.use(routes) make sure you put this routes last before any app.use()
+app.use(
+  session({
+    secret: "gogo", //simple secrt key
+    saveUninitialized: false, // this one have to do with sessin storage if you dont want to save unmodified session data to session storrage,
+    //for example a visiitor on your site and they not doing anything you dont want to save their session to eat alot of memory, best user authentication and managing user sessin
+    resave: false, // forcing a session save back to session store even if the sessin if self never modified at all
+    cookie: {
+      maxAge: 60000 * 60, // for session expiration
+    },
+  })
+);
 
 const loggingMiddleware = (request, response, next) => {
   console.log(`${request.method} --- ${request.url}  `);
@@ -22,33 +36,47 @@ const PORT = process.env.PORT || 3000;
 app.get("/", (request, response) => {
   // response.send('hellow world!')
   // response.send({msg: 'hello'})
-  //setting up time for cookie if this one expires, the authentication also expire so you cannot visit other roustes,because it expires 
+  //setting up time for cookie if this one expires, the authentication also expire so you cannot visit other roustes,because it expires
   //you can also signed a cookie and you need to tracke which cookis is signed or not
-  response.cookie('hello', 'world', {maxAge: 10000, signed: true}) 
+  console.log(request.session); //it logs cookie object session
+  console.log(request.session.id); //it logs session id this one will generate new everytime u make a request of thia api routes
+
+  request.session.visited = true; // this one will saves session id, so every you make a request the servel wiil know is request like an authneticaation then id will not change or genertaed every single time
+  //so they can attached the correct dta
+  request.sessionStore.get(request.session.id, (err, sessionData) =>{ //this one will give you the sessionData on whats happening, sessin id.
+    if(err) {
+      console.log(err);
+      throw err;
+    }
+    console.log(sessionData)
+  })
+  response.cookie("hello", "world", { maxAge: 60000 * 2 , signed: true });
   response.status(201).send({ msg: "ralph" });
 });
 
-const sampletObject = ([
-  {name: 'ralp', age: 40, location: 'navotas'},
-  {name: 'shenron', age: 10, location: 'malabon'},
-  {name: 'gadwin', age: 5, location: 'makati'},
-
-])
-// using cookie for sample authentication, 
-app.get('/api/sampleRoute/employee', (request, response) => {
+const sampletObject = [
+  { name: "ralp", age: 40, location: "navotas" },
+  { name: "shenron", age: 10, location: "malabon" },
+  { name: "gadwin", age: 5, location: "makati" },
+];
+// using cookie for sample authentication,
+app.get("/api/sampleRoute/employee", (request, response) => {
   // console.log(request.cookies)
-  console.log(request.headers.cookie)
- console.log(request.cookies)
-console.log(request.signedCookies.hello)
-//cookies and signedCookie are different yo need to track which is signed or not
- if(request.signedCookies.hello && request.signedCookies.hello === 'world' ) {
-  response.status(200).send(sampletObject)
-} else {
-  return response.status(403).send('you need the correct cookie')
-}
 
-})
+  console.log(request.session)
+  console.log(request.session.id)
 
+
+  // console.log(request.headers.cookie);
+  // console.log(request.cookies);
+  // console.log(request.signedCookies.hello);
+  //cookies and signedCookie are different yo need to track which is signed or not
+  if (request.signedCookies.hello && request.signedCookies.hello === "world") {
+    response.status(200).send(sampletObject);
+  } else {
+    return response.status(403).send("you need the correct cookie");
+  }
+});
 
 //localhost:3000/api/users
 // using middleware here, like chaining using next() then it will execute next middleware
@@ -66,13 +94,14 @@ app.get(
       { id: 2, name: "shenrron", location: "malabon" },
       { id: 3, name: "gadwin", location: "makati" },
     ]);
- next()
+    next();
   },
   () => {
-   console.log('ralph next() using') 
-   console.log(`using next() this this one consoliing the object ${sampletObject}`)
+    console.log("ralph next() using");
+    console.log(
+      `using next() this this one consoliing the object ${sampletObject}`
+    );
   }
-
 );
 
 //using .status
@@ -216,6 +245,55 @@ app.delete("/api/products/:id", (request, response) => {
   products.splice(findProductId, 1);
   return response.sendStatus(200);
 });
+//+++++++++++++++++++++++++++++++++++++++++++++++ session part 2
+// thi one i session logging in
+const objectNames = [
+  {username: 'ralph', password: '123456'},
+  {username: 'shenron', password: '123shenron'},
+  {username: 'gadwin', password: '123gadwin'},
+
+
+]
+
+app.post('/api/auth' , (request, response) => {
+const {body: {username, password}} = request;
+
+// reponse.send(objectNames)
+const findUser = objectNames.find((name) => name.username === username);
+if(!findUser || findUser.password !== password) return response.status(401).send({msg: 'BAD CREDENTIALS'})
+request.session.user = findUser;
+return response.status(200).send(findUser)
+})
+
+app.get('/api/auth/status', (request, response) => {
+  request.sessionStore.get(request.sessionID, (err, session) => {
+    console.log(session)
+  })
+  return request.session.user ? response.status(200).send(request.session.user)
+  : response.status(401).send({msg: 'NOT AUTHENTICATED'})
+})
+
+///++++++++++++++++++++++++++++++++++++
+//----------------------------
+//sample authe if log in uisng /api/auth
+
+app.post('/api/cart', (request, response) => {
+  if(!request.session.user) return response.sendStatus(401);
+  const {body: item} = request;
+  const {cart} = request.session;
+  if(cart) {
+    cart.push(item)
+  } else {
+    request.session.cart = [item]
+  }
+  return response.status(201).send(item)
+})
+
+app.get('/api/cart', (request, response) => {
+  if(!request.session.user) return response.sendStatus(401);
+  return response.send(request.session.cart ?? []);
+})
+//----------------------------
 
 app.listen(PORT, () => {
   console.log(`server is starting at  ${PORT}`);
